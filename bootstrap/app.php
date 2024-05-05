9dd940d3d65eb1f93dfba99d5b83c2b4528b3bf2
Denis <shevelevdenis91@gmail.com>
2024-05-05 19:42:20 +0300

    Vue forms, middlewares, flash messages

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
-            HandleInertiaRequests::class,
+            HandleInertiaRequests::class,// have to be last one
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -3,13 +3,23 @@
         <Link href="/">Index Page</Link>
         <Link href="/hello">Show Page</Link>
     </div>
-    <div>layout timer {{ counter }}</div>
+    <div v-if="flashSuccess" class="success">
+        {{ flashSuccess }}
+    </div>
     <slot>Default</slot>
 </template>
 
 <script setup>
-    import {ref} from 'vue'
-    import {Link} from '@inertiajs/vue3'
-    const counter = ref(0)
-    setInterval(() => counter.value++, 1000)
+    import {Link, usePage} from '@inertiajs/vue3'
+    import {computed} from 'vue'
+
+    const page = usePage();
+    const flashSuccess = computed(()=> page.props.flash.success)
 </script>
+
+<style scoped>
+.success{
+    background-color: green;
+    color: white;
+}
+</style>
