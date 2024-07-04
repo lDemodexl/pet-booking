7a7bfb0e6edd7e5a60191f17e2aea225bd447f05
Denis <shevelevdenis91@gmail.com>
2024-07-04 19:23:16 +0300

    sending Email and account verification by email

---
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -0,0 +1,19 @@
+<template>
+   <h2>You need to be verified to see this page!</h2>
+   <div>
+      <Link 
+         class="text-indigo-600 dark:text-indigo-400"
+         as="button" 
+         method="post" 
+         :href="route('verification.send')"
+      >
+            Resend verification email
+      </Link>
+   </div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+
+</script>
